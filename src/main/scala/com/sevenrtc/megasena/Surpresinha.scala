6e84eb2caea1504c73010bd09cdf4d643586d587
Anthony Accioly <a.accioly@7rtc.com>
2011-12-14 12:12:02 -0200

    Commit Inicial.

---
+++ src/main/scala/com/sevenrtc/megasena/Surpresinha.scala
@@ -0,0 +1,36 @@
+package com.sevenrtc.megasena
+
+import util.Random
+import collection.immutable.TreeSet
+
+/**
+ *
+ * @author Anthony Accioly
+ */
+object Surpresinha {
+
+  private val RANDOM = new Random
+
+  def main(args: Array[String]) {
+    for (i <- 1 to 10) {
+      printf("%-2d - %s\n", i, geraAposta mkString("[", ", ", "]"))
+    }
+  }
+
+  private def geraAposta = {
+
+    def getNumber = {
+      1 + RANDOM.nextInt(60)
+    }
+
+    def geraApostaRecursiva(aposta: Set[Int]): Set[Int] = {
+      if (aposta.size < 6)
+        geraApostaRecursiva(aposta + getNumber)
+      else
+        aposta
+    }
+
+    geraApostaRecursiva(TreeSet.empty[Int]);
+  }
+
+}
